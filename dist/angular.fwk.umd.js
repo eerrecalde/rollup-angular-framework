94d0bd8bfa19b109d0dd5b7ece4a446324db1e2e
Emiliano Errecalde <eerrecalde@gmail.com>
2017-09-18 18:38:00 +0100

    Testing tree shaking

--- dist/angular.fwk.umd.js
+++ dist/angular.fwk.umd.js
@@ -313,162 +313,162 @@ var asyncGenerator = function () {
   };
 }();
 
-var FilterIoCore = function filterIo() {
-  /**
-   * getObjectValueFromString - Process and returns the value of the property found from given string
-   *
-   * @param  {object} o object to look at
-   * @param  {string} s string to process
-   * @return {type}     returns the value of property found from the given string
-   */
+/**
+ * getObjectValueFromString - Process and returns the value of the property found from given string
+ *
+ * @param  {object} o object to look at
+ * @param  {string} s string to process
+ * @return {type}     returns the value of property found from the given string
+ */
 
-  /* eslint-disable */
-  function getObjectValueFromString(o, s) {
-    s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-    s = s.replace(/^\./, ''); // strip a leading dot
-    var a = s.split('.');
-    for (var i = 0, n = a.length; i < n; i += 1) {
-      var k = a[i];
-      if (k in o) {
-        o = o[k];
-      } else {
-        return;
-      }
+/* eslint-disable */
+function getObjectValueFromString(o, s) {
+  s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+  s = s.replace(/^\./, ''); // strip a leading dot
+  var a = s.split('.');
+  for (var i = 0, n = a.length; i < n; i += 1) {
+    var k = a[i];
+    if (k in o) {
+      o = o[k];
+    } else {
+      return;
     }
-    return o;
   }
-  /* eslint-enable */
+  return o;
+}
+/* eslint-enable */
 
-  /**
-   * isInArray - Returns true if the element is found in the array.
-   *
-   * @param  {string}  el        Item property string retrieved from the list of properties from each item
-   * @param  {string}  flt       Filter string retrieved from the list of filters
-   * @param  {boolean} isPartial Filters partially when true, or exact otherwise
-   * @return {boolean}           Returns true if the element is found in the array
-   */
-  function isInArray(el, flt, isPartial) {
-    return isPartial ? el.indexOf(flt) > -1 : el === flt;
+/**
+ * isInArray - Returns true if the element is found in the array.
+ *
+ * @param  {string}  el        Item property string retrieved from the list of properties from each item
+ * @param  {string}  flt       Filter string retrieved from the list of filters
+ * @param  {boolean} isPartial Filters partially when true, or exact otherwise
+ * @return {boolean}           Returns true if the element is found in the array
+ */
+function isInArray(el, flt, isPartial) {
+  return isPartial ? el.indexOf(flt) > -1 : el === flt;
+}
+
+/**
+ * getLowrCaseOrNumber - Returns the same value, but converted into float if number, or lowercase if string
+ * This is to facilitate the comparission
+ *
+ * @param  {any} value to process
+ * @return {type} processed value
+ */
+function getLowerCaseOrNumber(el) {
+  if (typeof el === 'number') {
+    return parseFloat(el);
   }
 
-  /**
-   * getLowrCaseOrNumber - Returns the same value, but converted into float if number, or lowercase if string
-   * This is to facilitate the comparission
-   *
-   * @param  {any} value to process
-   * @return {type} processed value
-   */
-  function getLowerCaseOrNumber(el) {
-    if (typeof el === 'number') {
-      return parseFloat(el);
+  if (typeof el === 'string') {
+    // eslint-disable-next-line
+    return isNaN(el) ? el.toLowerCase() : parseFloat(el);
+  }
+
+  return el;
+}
+
+/**
+ * atLeastOneMatch - Returns true if 1 of the elements matches the criteria
+ *
+ * @param  {array}   filters    List of filters match against
+ * @param  {string}  el         Element to look for in the filters array
+ * @param  {boolean} isPartial  If true, the match rule will be equal|contained in. If false, it'll be equal.
+ * @return {boolean}            Returns boolean for element found condition
+ */
+function atLeastOneMatch(fltrs, element, isPartial) {
+  var counter = 0;
+  var filters = fltrs;
+  var el = element;
+
+  // Unify filters. Make it an array if it isn't yet
+  if (!Array.isArray(fltrs)) {
+    filters = [fltrs];
+  }
+
+  filters.forEach(function (filter) {
+    var flt = filter;
+    // VALIDATIONS
+
+    flt = getLowerCaseOrNumber(flt);
+    el = getLowerCaseOrNumber(el);
+
+    if ((typeof el === 'undefined' ? 'undefined' : _typeof(el)) !== (typeof flt === 'undefined' ? 'undefined' : _typeof(flt))) {
+      return;
     }
 
-    if (typeof el === 'string') {
+    if (el === flt) {
+      counter += 1;
+      return;
+    }
+
+    if (!filters.length || el && el.length && isInArray(el, flt, isPartial)) {
+      counter += 1;
       // eslint-disable-next-line
-      return isNaN(el) ? el.toLowerCase() : parseFloat(el);
+      return;
     }
+  });
 
-    return el;
+  return !!counter;
+}
+
+/**
+ * var getFiltredCollection - Filters given array against given filters.
+ *
+ * @param  {array} items   Array to loop through
+ * @param  {array} filters Filters to be used to remove or not elements from the resulting array
+ * @return {array}         Final filtred array
+ */
+function getFiltredCollection(items, filters, isPartial) {
+  if (!filters || !filters.length) {
+    return items;
   }
 
-  /**
-   * atLeastOneMatch - Returns true if 1 of the elements matches the criteria
-   *
-   * @param  {array}   filters    List of filters match against
-   * @param  {string}  el         Element to look for in the filters array
-   * @param  {boolean} isPartial  If true, the match rule will be equal|contained in. If false, it'll be equal.
-   * @return {boolean}            Returns boolean for element found condition
-   */
-  function atLeastOneMatch(fltrs, element, isPartial) {
-    var counter = 0;
-    var filters = fltrs;
-    var el = element;
+  console.log('getFiltredCollection');
 
-    // Unify filters. Make it an array if it isn't yet
-    if (!Array.isArray(fltrs)) {
-      filters = [fltrs];
-    }
+  return items.filter(function (j) {
+    var ioObjectMatchingCounter = 0;
 
-    filters.forEach(function (filter) {
-      var flt = filter;
-      // VALIDATIONS
+    return j.data.io.filter(function (item) {
 
-      flt = getLowerCaseOrNumber(flt);
-      el = getLowerCaseOrNumber(el);
+      filters.forEach(function (filterUnit) {
+        var filtersMatchingCounter = 0;
+        var filterKeys = Object.keys(filterUnit);
 
-      if ((typeof el === 'undefined' ? 'undefined' : _typeof(el)) !== (typeof flt === 'undefined' ? 'undefined' : _typeof(flt))) {
-        return;
-      }
+        filterKeys.forEach(function (key) {
+          var propInItem = item[key] || undefined;
 
-      if (el === flt) {
-        counter += 1;
-        return;
-      }
-
-      if (!filters.length || el && el.length && isInArray(el, flt, isPartial)) {
-        counter += 1;
-        // eslint-disable-next-line
-        return;
-      }
-    });
-
-    return !!counter;
-  }
-
-  /**
-   * var getFiltredCollection - Filters given array against given filters.
-   *
-   * @param  {array} items   Array to loop through
-   * @param  {array} filters Filters to be used to remove or not elements from the resulting array
-   * @return {array}         Final filtred array
-   */
-  function getFiltredCollection(items, filters, isPartial) {
-    if (!filters || !filters.length) {
-      return items;
-    }
-
-    return items.filter(function (j) {
-      var ioObjectMatchingCounter = 0;
-
-      return j.data.io.filter(function (item) {
-
-        filters.forEach(function (filterUnit) {
-          var filtersMatchingCounter = 0;
-          var filterKeys = Object.keys(filterUnit);
-
-          filterKeys.forEach(function (key) {
-            var propInItem = item[key] || undefined;
-
-            if (!item[key] && getObjectValueFromString(item, key) !== undefined) {
-              propInItem = getObjectValueFromString(item, key);
-            }
-
-            if (key === 'undefined' || filterUnit[key] === 'undefined') {
-              return;
-            }
-
-            if (propInItem && atLeastOneMatch(filterUnit[key], propInItem, isPartial)) {
-              filtersMatchingCounter += 1;
-            }
-
-            // eslint-disable-next-line
-            return;
-          });
-
-          if (filtersMatchingCounter === filterKeys.length) {
-            ioObjectMatchingCounter += 1;
+          if (!item[key] && getObjectValueFromString(item, key) !== undefined) {
+            propInItem = getObjectValueFromString(item, key);
           }
+
+          if (key === 'undefined' || filterUnit[key] === 'undefined') {
+            return;
+          }
+
+          if (propInItem && atLeastOneMatch(filterUnit[key], propInItem, isPartial)) {
+            filtersMatchingCounter += 1;
+          }
+
+          // eslint-disable-next-line
+          return;
         });
 
-        return ioObjectMatchingCounter === filters.length;
-      }).length > 0;
-    });
-  }
+        if (filtersMatchingCounter === filterKeys.length) {
+          ioObjectMatchingCounter += 1;
+        }
+      });
 
-  return {
-    getFiltredCollection: getFiltredCollection
-  };
-}();
+      return ioObjectMatchingCounter === filters.length;
+    }).length > 0;
+  });
+}
+
+var FilterIoCore = {
+  getFiltredCollection: getFiltredCollection
+};
 
 // Tree Shaking test. I'm including this, but not using.
 // Rollup is smart enough to leave this out the bundle.
