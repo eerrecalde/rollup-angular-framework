6025602efcd26ec0a51fe53940ead2b3e06a88b5
Emiliano Errecalde <eerrecalde@gmail.com>
2017-11-08 12:48:49 +0000

    Renamed folders

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default [
 
 	// CommonJS (for Node) and ES module (for bundlers) build.
 	{
-		input: 'lib/index.js',
+		input: 'src/index.js',
 		output: [
 			// { file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' },
